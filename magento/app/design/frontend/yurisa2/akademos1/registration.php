4fa6c454d58ffacad6f1df4c2a2e9aaded5c8def
@yurisa2 MT5 <yuri@sa2.com.br>
2018-10-26 15:46:22 -0300

    Functional Wiped version Bitnami OVA

---
+++ magento/app/design/frontend/yurisa2/akademos1/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/yurisa2/akademos1',
+    __DIR__
+); 
